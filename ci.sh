4b814df62730ecd8647727876618372ea8a1ddd3
Clemens Akens <clebert@me.com>
2024-02-15 10:11:15 +0100

    Update config

--- ci.sh
+++ ci.sh
@@ -3,9 +3,9 @@ set -e           # Terminates script at the first error
 set -o pipefail  # Sets the exit status for pipes
 set -u           # Triggers an error when an unset variable is called
 set -o noclobber # Prevents from overwriting existing files
-npm run format:check
-npm run lint
 npm run compile:emit
 npm run compile:check
+npm run format:check
+npm run lint
 npm test
 npm start
